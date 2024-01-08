ac4ed3ba7e0fe0d25ee97b8aad64c971a8fb72d2
Matheus Fré <matheusfre94@gmail.com>
2024-01-08 16:29:31 -0300

    criando os temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from "styled-components";
+
 import Sobre from "./containers/About";
 import Projetos from "./containers/Projects";
 import SideBar from "./containers/SideBar";
 import EstiloGlobal, { Container } from "./styles";
+import temaLight from "./themes/light";
+import temaDark from "./themes/dark";
+import { useState } from "react";
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false);
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark);
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <SideBar />
+        <SideBar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
